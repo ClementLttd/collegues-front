40fcde5c866a8c1012a068f723a98344c7f196ea
ClementLttd <ClementLttd>
2020-02-05 09:08:15 +0100

    collegues tp2

---
+++ src/app/mock/collegues.mock.ts
@@ -0,0 +1,5 @@
+import { Collegue } from '../models/Collegue';
+
+const collegueMock: Collegue = new Collegue('Littardi', 'bchjakbchvdzjk', 'Clement', 'vfdjkl@gmail.com', new Date(2020, 1, 1), 'vnsjdklvn');
+
+export { collegueMock };
